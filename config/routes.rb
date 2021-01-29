4eda0a314e9f914dd16b7624fb3bf48debadcb13
Manuel Bustillo <mayn13@gmail.com>
2021-01-29 09:55:44 +0100

    Define the events#new action as the default route

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq' if Rails.env.development?
   resources :events
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'events#new'
 end
