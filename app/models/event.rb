ad573e3e6ad30185a1e475f8af85d17304bad944
Manuel Bustillo <mayn13@gmail.com>
2021-01-29 10:22:26 +0100

    Import session names

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,29 @@
+require 'csv'
+
 class Event < ApplicationRecord
   has_many :people, dependent: :destroy
   has_many :preferences, through: :people
   has_many :sessions, dependent: :destroy
   has_many :executions
+  attr_accessor :import
+
+  before_save :import_data
+
+  private
+
+  def import_data
+    return unless import.is_a?(ActionDispatch::Http::UploadedFile)
+
+    csv = CSV.read(import, headers: true)
+
+    ActiveRecord::Base.transaction do
+      clear_data
+      session_names = csv.headers.compact
+      session_names.each { |name| sessions.create(name: name) }
+    end
+  end
+
+  def clear_data
+    sessions.delete_all
+  end
 end
