da2f22be22bd1110f4afde7d8e7bff25c5415f1b
Manuel Bustillo <mayn13@gmail.com>
2021-01-29 10:01:08 +0100

    Define the events#index action as the default route

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq' if Rails.env.development?
   resources :events
-  root to: 'events#new'
+  root to: 'events#index'
 end
