e94e68c5e5b53bc7d3db37ea194e990b63f5fa12
Manuel Bustillo <mayn13@gmail.com>
2021-01-30 14:35:03 +0100

    Add capacity to create multiple executions

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq' if Rails.env.development?
   resources :events do
-    resources :executions, only: :show
+    resources :executions, only: [:show, :create]
   end
   root to: 'events#index'
 end
