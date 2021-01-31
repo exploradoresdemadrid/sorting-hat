7488c73a68ddae5a5abd083a645c6899d016503f
Manuel Bustillo <mayn13@gmail.com>
2021-01-31 11:23:07 +0100

    Temporarily enable sidekiq in production

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  mount Sidekiq::Web => '/sidekiq' if Rails.env.development?
+  mount Sidekiq::Web => '/sidekiq'
   resources :events do
     resources :executions, only: [:show, :create]
   end
